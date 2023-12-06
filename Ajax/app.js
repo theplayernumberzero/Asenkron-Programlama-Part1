456699f81179b10bfcf3bcd767c3f30a829b899f
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2023-12-06 17:47:52 +0300

    ajax

--- Ajax/app.js
+++ Ajax/app.js
@@ -7,13 +7,39 @@
 
 //ReadyStates -->Hold status of XMLHttpRequest
 // 0	UNSENT	Client has been created. open() not called yet.
-// 1	OPENED	open() has been called.
-// 2	HEADERS_RECEIVED	send() has been called, and headers and status are available.
-// 3	LOADING	Downloading; responseText holds partial data.
-// 4	DONE	The operation is complete.
+// 1	server connection established.
+// 2	request received.
+// 3	processing request
+// 4	req finished, response is ready
 
 document.getElementById("btn").addEventListener("click",function(){
     //ajax için XMLHttpRequest objesi oluşturmalıyız.
     const xhr = new XMLHttpRequest();
 
+    //bağlantı açıyoruz
+    xhr.open("GET","example.txt",true);     //Request tipi, veri alınacak yer, async mi
+
+    //ESKİ YÖNTEM
+    // xhr.onreadystatechange = function(){    //Ready state değiştikçe çalışır
+
+    //     console.log(this.readyState);  //xhr içindeyiz
+    //     console.log(this.status);   //hhtp status code
+
+    //     if(this.status == 200 && this.readyState == 4){     //Response un hazır olup olmadığı böyle kontrol edilir.
+    //         console.log(this.responseText); //yazdırma
+    //     }
+    // }
+
+    //response hazır olunca çalışır
+    xhr.onload = function(){ //readyState = 4 olunca çalışır
+        if(this.status == 200){
+            document.getElementById("ajax").textContent = this.responseText;    //içeriği tet olarak aldık
+        }
+    }
+
+    xhr.onprogress = function(){    //readyState = 3 olunca çalışır
+        console.log("Process işleniyor");
+    }
+
+    xhr.send();     //POST req yapsaydık string değer girmek zorunda kalacaktık.
 });
