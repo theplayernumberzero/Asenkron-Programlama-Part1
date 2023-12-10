2f3a372bbe153d98fa1a7a1de7412c44f4b3d02b
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2023-12-10 22:08:30 +0300

    post request

--- Ajax ile HTTP Requestleri/app.js
+++ Ajax ile HTTP Requestleri/app.js
@@ -24,6 +24,27 @@ class Request{
 
         this.xhr.send();    //Request gönderildi
     }
+
+    //POST Request
+
+    post(url, data, callback){
+
+        this.xhr.open("POST",url);
+
+        this.xhr.setRequestHeader("Contect-type","application/json");   //veriyi nasıl göndereceğimizi belirtiyoruz (form verisi mi json mu belirtme yeri)
+
+        this.xhr.onload = () => {
+            if(this.xhr == 201){    //Post için 201 status code u
+
+                callback(null,this.xhr.responseText);   //Oluşturulan kaynak response olarak gelecek
+            }
+            else{
+                callback("Bir hata oluştu..",null);
+            }
+        };
+
+        this.xhr.send(JSON.stringify(data));
+    }
 }
 
 const request = new Request();
@@ -35,5 +56,15 @@ request.get("https://jsonplaceholder.typicode.com/albums",function(error,respons
     }
     else{
         console.log(error);
+    }
+});
+
+request.post("https://jsonplaceholder.typicode.com/albums",{userid:2,title:"CR7"}, function(err, album){    //JSON a object i stringfy ile dönüştürerek gönderiyoruz
+
+    if(error === null){
+        console.log(album);
+    }
+    else{
+        console.log(err);
     }
 });
